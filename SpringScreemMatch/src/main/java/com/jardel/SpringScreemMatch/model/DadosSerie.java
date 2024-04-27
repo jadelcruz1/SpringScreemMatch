9b693654163dad81908abf16147f143df9e26be4
jadelcruz1 <jardelcruz6@gmail.com>
2024-04-27 18:15:04 -0300

    inclusão da biblioteca jackson no projeto e criação do Record (DTO) no projeto

---
+++ SpringScreemMatch/src/main/java/com/jardel/SpringScreemMatch/model/DadosSerie.java
@@ -0,0 +1,10 @@
+package com.jardel.SpringScreemMatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(
+        @JsonAlias("Title") String titulo,
+        @JsonAlias("totalSeasons") Integer totalTemporadas,
+        @JsonAlias("imdbRating") String avaliacao
+) {
+}
