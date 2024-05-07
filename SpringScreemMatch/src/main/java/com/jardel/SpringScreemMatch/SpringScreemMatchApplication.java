c48abf2e52e189343596aa7614fa91af12f0db4c
jadelcruz1 <jardelcruz6@gmail.com>
2024-05-07 18:24:11 -0300

    Modelagem da classe Principal.java

--- SpringScreemMatch/src/main/java/com/jardel/SpringScreemMatch/SpringScreemMatchApplication.java
+++ SpringScreemMatch/src/main/java/com/jardel/SpringScreemMatch/SpringScreemMatchApplication.java
@@ -1,10 +1,8 @@
 package com.jardel.SpringScreemMatch;
 
-import com.jardel.SpringScreemMatch.model.DadosEpisodios;
-import com.jardel.SpringScreemMatch.model.DadosSerie;
 import com.jardel.SpringScreemMatch.model.DadosTemporada;
+import com.jardel.SpringScreemMatch.principal.Principal;
 import com.jardel.SpringScreemMatch.service.ConsumoAPI;
-import com.jardel.SpringScreemMatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,28 +19,9 @@ public class SpringScreemMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi  = new ConsumoAPI();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=910a96b4&t=Gilmore+Girls");
-
-		System.out.println(json);
-
-		ConverteDados conversor  = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println("Séries " + dados);
+		Principal principal = new Principal();
+		principal.exibirMenu();
 
 
-		json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=910a96b4&t=Gilmore+Girls&season=1&episode=2");
-		DadosEpisodios dadosEpisodio = conversor.obterDados(json, DadosEpisodios.class);
-		System.out.println(dadosEpisodio);
-
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i =1; i<=dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=910a96b4&t=Gilmore+Girls&season=" + i);
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
 	}
 }
--- SpringScreemMatch/src/main/java/com/jardel/SpringScreemMatch/principal/Principal.java
+++ SpringScreemMatch/src/main/java/com/jardel/SpringScreemMatch/principal/Principal.java
@@ -1,15 +1,24 @@
 package com.jardel.SpringScreemMatch.principal;
 
+import com.jardel.SpringScreemMatch.model.DadosSerie;
+import com.jardel.SpringScreemMatch.model.DadosTemporada;
 import com.jardel.SpringScreemMatch.service.ConsumoAPI;
+import com.jardel.SpringScreemMatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoAPI consumo = new ConsumoAPI();
 
-    private final String ENDERECO = "https://www.omdbapi.com/?";
-    private final String API_KEY =  "apikey=910a96b4";
+    private final String ENDERECO = "https://www.omdbapi.com/";
+    private final String API_KEY =  "?apikey=910a96b4&t=";
+
+    private ConverteDados conversor  = new ConverteDados();
+
+    //json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=910a96b4&t=Gilmore+Girls&season=" + i);
 
 
 
@@ -18,8 +27,21 @@ public class Principal {
 
         var nomeSerie = leitura.nextLine();
 
+        var json = consumo.obterDados(ENDERECO + API_KEY + nomeSerie.replace(" ", "+"));
 
-        var json = consumo.obterDados(ENDERECO + API_KEY + nomeSerie.replace(" ", " + "));
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i =1; i<=dados.totalTemporadas(); i++){
+            ConsumoAPI consumoApi;
+            json = consumo.obterDados(ENDERECO + API_KEY + nomeSerie.replace(" ", "+") + "&season=" + i );
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+
 
 
         //json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=910a96b4&t=Gilmore+Girls&season=" + i);
